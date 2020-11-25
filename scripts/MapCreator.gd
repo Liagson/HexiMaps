4ae0df9b30cecc847969b80e241161accb8f1bc8
Alberto Aliaga <aliaga.alberto@gmail.com>
2020-11-25 22:30:06 +0100

    Refactoring tile geo info selection

--- scripts/MapCreator.gd
+++ scripts/MapCreator.gd
@@ -1,38 +1,41 @@
 extends Node
 
 const Tile = preload("res://Tile.tscn")
-onready var noise = $Noise
+const TileGeoInfo = preload("res://scripts/TileGeoInfo.gd")
+onready var heightNoise = $HeightNoise
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var noiseValue = null
 	var tile = null
+	var tileGeoTipe = null
 	var tileSize = Tile.instance().get_node("Background").texture.get_size()
 	var geoMap = get_node("../GeoMap")
-
+	
 	for y in range(12):
 		for x in range(15):
-			noiseValue = noise.texture.noise.\
+			var heightNoiseValue = heightNoise.texture.noise.\
 			get_noise_2d(x * tileSize[0], y * tileSize[1])
 			tile = Tile.instance()
-			tile.get_node("Background").texture = getTileTexture(noiseValue)
+			tileGeoTipe = getTileGeoTipe(heightNoiseValue)
+			tile.get_node("Background").texture = getTileGeoTexture(tileGeoTipe)
 			tile.position = getTilePosition(x, y, tileSize)
 			geoMap.add_child(tile)
-			
 
-func getTileTexture(heightValue):
-	# todo: el parametro de entrada deberia de ser un enum
-	# mete la logica de decidir en otro metodo. maybe
+func getTileGeoTipe(heightValue):
 	if heightValue <= -0.75:
-		return load("res://tiles/pngs/deep_sea.png")
+		return TileGeoInfo.TileTipe.deepSea
 	elif heightValue > -0.75 and heightValue <= -0.25:
-		return load("res://tiles/pngs/shallow_sea.png")
+		return TileGeoInfo.TileTipe.shallowSea
 	elif heightValue > -0.25 and heightValue <= 0.25:
-		return load("res://tiles/pngs/green_field.png")
+		return TileGeoInfo.TileTipe.field_green
 	elif heightValue > 0.25 and heightValue <= 0.5:
-		return load("res://tiles/pngs/hills_(green).png")
+		return TileGeoInfo.TileTipe.hills_green
 	else:
-		return load("res://tiles/pngs/mountains.png")
+		return TileGeoInfo.TileTipe.mountains
+
+func getTileGeoTexture(geoTipe):
+	return load(TileGeoInfo.tileTextures[geoTipe])
 
 func getTilePosition(relativeX, relativeY, tileSize):
 	var tileWidth = tileSize[0]
---
+++ scripts/TileGeoInfo.gd
@@ -0,0 +1,15 @@
+enum TileTipe {
+	deepSea,
+	shallowSea,
+	field_green,
+	hills_green,
+	mountains
+}
+
+const tileTextures = {
+	TileTipe.deepSea : "res://tiles/pngs/deep_sea.png",
+	TileTipe.shallowSea : "res://tiles/pngs/shallow_sea.png",
+	TileTipe.field_green : "res://tiles/pngs/green_field.png",
+	TileTipe.hills_green : "res://tiles/pngs/hills_(green).png",
+	TileTipe.mountains : "res://tiles/pngs/mountains.png"
+}
